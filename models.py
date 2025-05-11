b2e705e5966b8c21c6fdbebb8c98be430002d442
Audrius Molis <vienas00@gmail.com>
2025-05-11 17:06:32 +0300

    Try more models

--- models.py
+++ models.py
@@ -67,3 +67,24 @@ class BiTLikeModel(nn.Module):
         x = torch.flatten(x, 1)
         x = self.fc(x)
         return x
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SimpleConvNet(nn.Module):
+    def __init__(self, num_classes):
+        super(SimpleConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32 * 56 * 56, 64)  # Adjusted input size for 224x224 input
+        self.fc2 = nn.Linear(64, num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
